884d2df519fb703e03b3724dc8b631c0f3402aa6
Charity3191 <munezerocharity0@gmail.com>
2024-09-19 18:55:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+$name="MUNEZERO CHARITY";
+$DOB="05/11/2003";
+$Address="KISORO";
+$MYear=2024;
+$BYear=2003;
+$Age=$MYear-$BYear;
+echo"Name: ".$name. "<br>";
+echo"Date Of Birth : ".$DOB."<br>";
+echo"Home Address: ".$Address."<br>";
+echo"Welcome home, ".$name."<br>";
+echo"You are now , ".$Age."Years old";
+/* DOB stands for date of birth,
+MYEAR stands for current year
+BYEAR stands for year of birth*/
+?> 
+
